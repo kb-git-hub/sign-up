9f4953bd1242c2fab1ef8e0189ce22048f47cf57
kj.br <kj.br@proton.me>
2022-08-26 16:25:14 -0500

    Pass Validation

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
 const password = document.querySelector('#password')
 const passwordConfirm = document.querySelector('#confirm-password')
 const submitBtn = document.querySelector('button')
+const passMatch = document.querySelector('#passMatch')
 
-function checkPassword(event) {
-    e.preventDefault()
-    if (password.value || passwordConfirm.value !== undefined) {
-        if (password.value !== confirm.passwordConfirm.value) {
-            alert('Passwords do NOT match!')
-            password.value = ''
-            passwordConfirm.value = ''
-        }
-    } return
+function checkPassword() {
+    if (passMatch.style.display = 'none') {
+        passMatch.style.display = 'inline-block'
+        passMatch.innerText = "Passwords don't match"
+        password.value = ''
+        passwordConfirm.value = ''
 
+
+        setTimeout(() => {
+            passMatch.style.display = 'none'
+        }, 2000);
+    }
 }
 
-submitBtn.addEventListener('click', (e) =>{
+submitBtn.addEventListener('click', (e) => {
     if (password.value || passwordConfirm.value) {
         if (password.value !== passwordConfirm.value) {
             e.preventDefault()
-            alert('Passwords do NOT match!')
-            password.value = ''
-            passwordConfirm.value = ''
+            checkPassword()
+
         }
-    } 
-    
+    }
+
 })
 
 
