e8cb93c064d12dddc82249b03621e138be7f3aa1
KB <kj.br@protonmail.com>
2022-08-31 11:43:37 -0500

    Update resizing feature

--- three.js
+++ three.js
@@ -205,3 +205,8 @@ addEventListener('mousemove', (e) => {
 })
 
 
+addEventListener('resize', ()=>{
+    camera.aspect = innerWidth/innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(innerWidth, innerHeight)
+})
